df7435a855f2b6d802ab620723ba96b004658035
Rafael leite <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-04-30 14:02:55 -0300

    v 1.0.3

--- Scripts/main.js
+++ Scripts/main.js
@@ -5,9 +5,9 @@
     var spinner = function () {
         setTimeout(function () {
             if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show', 1200);
+                $('#spinner').removeClass('show');
             }
-        }, 1000);
+        }, 600);
     };
     spinner();
     
