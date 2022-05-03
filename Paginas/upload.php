77ad365d3981ccc02239f2fffbfbb5cd04c06440
Rafael leite <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-05-03 20:58:07 -0300

    V 1.1.0

--- Paginas/upload.php
+++ Paginas/upload.php
@@ -8,5 +8,5 @@ if(isset($_FILES['files']['name'][0])){
         if(move_uploaded_file($_FILES['file']['tmp_name'][$key], $file_name))
         $result.="<div class=\"image\"><img src=\"${file_name}\" /</div>";
     }
-    echo $result;
 }
+echo $result;
