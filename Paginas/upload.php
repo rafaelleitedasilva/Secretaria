f4bec0d2883c0e652fcd8d93c940ea929631f7ad
Rafael leite <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-05-03 20:57:04 -0300

    V 1.0.9

--- Paginas/upload.php
+++ Paginas/upload.php
@@ -2,10 +2,10 @@
 
 $result='';
 if(isset($_FILES['files']['name'][0])){
-    foreach($_FILES['file']['name'] as $key => $files){
-        $file_name = 'upload/'.$_Files['file']['name'][$key];
+    foreach($_FILES['file']['name'] as $key => $file){
+        $file_name = 'upload/'.$_FILES['file']['name'][$key];
 
-        if(move_uploaded_file($_Files['file']['tmp_name'][$key], $file_name))
+        if(move_uploaded_file($_FILES['file']['tmp_name'][$key], $file_name))
         $result.="<div class=\"image\"><img src=\"${file_name}\" /</div>";
     }
     echo $result;
