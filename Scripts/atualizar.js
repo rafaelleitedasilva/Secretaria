3099ada363cdc051c252d3e2c0dcb738f0124cb5
Rafael leite <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-05-08 16:03:12 -0300

    v 1.2.9

--- Scripts/atualizar.js
+++ Scripts/atualizar.js
@@ -2,7 +2,7 @@ let x = 6;
 
 
 function atualizar(){
-    let update = document.getElementById('updates')
+    let update = document.getElementById('Updates')
     let data = document.getElementById('data')
     let autor = document.getElementById('autor')
     let tbody = document.getElementById('tbody')
@@ -18,13 +18,13 @@ function atualizar(){
 
 
     let tdTwo = document.createElement('td')
-    tdTwo.innerText = String(update.value);
+    tdTwo.innerHTML = String(update.value);
 
     let tdThree = document.createElement('td')
     tdThree.innerText = String(data.value);
 
     let tdFour = document.createElement('td')
-    tdFour.innerText = String(autor.value);
+    tdFour.innerHTML = String(autor.value);
 
     tbody.appendChild(tr)
     tr.appendChild(th)
