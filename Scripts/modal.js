6c07a567c3bc9288a09468158d58880ab35131a3
Rafael leite <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-05-08 01:48:12 -0300

    v 1.2.7

--- Scripts/modal.js
+++ Scripts/modal.js
@@ -6,64 +6,19 @@ function input(){
 
 function modal(fileInput){
     let rowUpload = document.getElementById("row")
-    fileInput.save
-    let files = fileInput.files
-
     let colNova = document.createElement("div.col")
+    let files = fileInput.files
     let imgNova = [];   
 
-    for(var i = 0; i < files.length; i++){
+    for(var i = -1; i < files.length; i++){
         imgNova[i] = document.createElement("img")
-        imgNova[i].src = URL.creatObjectURL(Event.target.files[0])
+      /*   console.log(files[i].name)
+        let a = new URL(`/${files[i].name}`, "https://github.com/rafaelleitedasilva")
+        console.log(a)
+        let url = URL.createObjectURL(a) */
+        imgNova[i].src = `../Imagens/Fotos/3.jpeg`
         imgNova[i].className="fotosAlbum"
         rowUpload.appendChild(colNova)
-        colNova.appendChild(imgNova[i])
+        colNova.appendChild(imgNova[0])
     }
 }
-         
- 
-
-
-
-/* ;(function modal(){
-
-    //Pega os elementos da tela e declara como constantes
-    const leitorDeArquivos = new FileReader(),
-            formulario = document.querySelector('.upload-imagem'),
-            previaDaImagem = document.querySelector('.imagem'),
-            inputArquivo = document.querySelector('.upload'),
-            btnAtualiza = document.querySelector('.btnAtualiza');
-
-    
-    function leEAtualiza(){
-        //pega o arquivo enviado e guarda nesta variavel
-        let imagemEnviada = inputArquivo.files[0];
-
-        //Usa a função do objeto leitor de arquivos, que lê o arquivo, e consegue reaproveita-lo para usar o arquivo como uma URL
-        leitorDeArquivos.readAsDataURL(imagemEnviada);
-        
-        //Após leitura da imagem (evento load), a função de callback define o valor do src da imagem de prévia com o valor do resultado da leitura do leitor de arquivos
-        leitorDeArquivos.addEventListener('loadend', function(load){
-            
-            //veja no console o que o resultado do leitor de arquivos :)
-            console.log(load.target.result);
-            //define o caminho da imagem com o caminho criado pelo leitor de arquivos
-            previaDaImagem.src = load.target.result
-        })
-    }
-
-    //Quando o formulário for enviado:
-    formulario.addEventListener('submit', function(submit){
-        //empede o recarregamento da página
-        submit.preventDefault();
-
-        leEAtualiza();
-    })
-
-    //Ou quando clica no boão atualizar
-    btnAtualiza.addEventListener('click', function(){
-        leEAtualiza();
-    })
-    
-
-})() */
