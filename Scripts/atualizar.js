a719c13fe0431f7303c76cc479b84344ba6213e5
Rafael leite <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-05-08 16:00:27 -0300

    v 1.2.8

---
+++ Scripts/atualizar.js
@@ -0,0 +1,34 @@
+let x = 6;
+
+
+function atualizar(){
+    let update = document.getElementById('updates')
+    let data = document.getElementById('data')
+    let autor = document.getElementById('autor')
+    let tbody = document.getElementById('tbody')
+
+    let tr = document.createElement('tr')
+    tr.className = 'hide'
+    
+    let th = document.createElement('th')
+    th.scope = 'row'
+    th.innerHTML = x;
+    x++;
+
+
+
+    let tdTwo = document.createElement('td')
+    tdTwo.innerText = String(update.value);
+
+    let tdThree = document.createElement('td')
+    tdThree.innerText = String(data.value);
+
+    let tdFour = document.createElement('td')
+    tdFour.innerText = String(autor.value);
+
+    tbody.appendChild(tr)
+    tr.appendChild(th)
+    tr.appendChild(tdTwo)
+    tr.appendChild(tdThree)
+    tr.appendChild(tdFour)
+}
