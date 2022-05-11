b26d44a6349b7e7d04774edbf93bfcb3cb6c11d5
Rafael leite <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-05-10 21:37:21 -0300

    v 1.4.2

---
+++ Scripts/login.js
@@ -0,0 +1,13 @@
+function validar(){
+    let email = document.getElementById('email')
+    let senha = document.getElementById('senha')
+    let link = document.getElementById('link')
+
+    if(String(email.value) == "secretariaMDES@senai.com" && String(senha.value) == "Nota100!" ){
+     
+            link.href = "index.html"
+
+    }else{
+        link.href = "#"
+    }
+}
