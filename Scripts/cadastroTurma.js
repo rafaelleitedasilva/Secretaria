cf4df89542ccd0846cb7037a829aaa2e40887534
Rafael leite <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-05-14 10:24:11 -0300

    v 2.6

--- Scripts/cadastroTurma.js
+++ Scripts/cadastroTurma.js
@@ -12,37 +12,80 @@ let vagas = document.getElementById("vagas")
 
 
 function cadastro(){
+
+    //Criando os Elementos
     let view = document.getElementById('visualizar')
+
     let hr = document.createElement('hr')
+
+    let br = document.createElement('br')
+
     let divCadastro = document.createElement('div')
+
     let h1Curso = document.createElement('h1')
-    let escola = document.createElement('p')
-    var valueEscola = escola.options[escola.selectedIndex].value;
+
+    let pescola = document.createElement('p')
+
+    let dataI = document.createElement('p')
+
+    let dataF = document.createElement('p')
+
     let pCodigo = document.createElement('p')
-    let pCodigo2 = document.createElement('p') 
 
-    
+    let pCodigo2 = document.createElement('p')
 
+    let pcarga = document.createElement('p')
+
+    let pvagas = document.createElement('p')
+
+
+    //Adicionando a div Cadastro na Div Visualização
+    view.appendChild(divCadastro)
+    //__________________________________________________
+
+    //Definindo alguns estilos
     h1Curso.className = 'titulo'
     divCadastro.className = 'Cadastro'
-    pCodigo.style = 'display: inline-block;'
-    pCodigo2.style = 'display: inline-block;'
 
 
+    //__________________________________________________
+    //Adicionando o nome do curso
     h1Curso.innerHTML = `${String(curso.value)}`
-    pCodigo.innerHTML = `Código: ${String(codigo.value)}`
-    pCodigo2.innerHTML = ` |Código de Identificação: ${String(codigoIdentificacao.value)}`
-    escola.innerHTML = `${String(valueEscola)}`
-
-    view.appendChild(divCadastro)
     divCadastro.appendChild(h1Curso)
-    divCadastro.appendChild(escola)
-    
-    divCadastro.appendChild(pCodigo)
+    //__________________________________________________
+
+    //Adicionando a Escola
+    pescola.innerHTML = `${$('#escola').val()}`
+    divCadastro.appendChild(pescola)
+
+    //__________________________________________________
+    //Adicionando os códigos
+    pCodigo2.innerHTML = ` Código de Identificação: ${String(codigoIdentificacao.value)}`
     divCadastro.appendChild(pCodigo2)
 
-    divCadastro.appendChild()
-    divCadastro.appendChild()
+    pCodigo.innerHTML = `Código: ${String(codigo.value)} ` + ' '
+    divCadastro.appendChild(pCodigo)
+
+    
+
+    //Adicionando Br
+    divCadastro.appendChild(br)
+
+    //Datas de Inicio e Final
+    dataI.innerHTML = `Data Inicial: ${$('#datainicial').val()}`
+    divCadastro.appendChild(dataI)
+    dataF.innerHTML = `Data Final: ${$('#datafinal').val()}`
+    divCadastro.appendChild(dataF)
+
+    //Adicionando a Carga Horária
+    pcarga.innerHTML = `Carga Horária: ${String(carga.value)}`
+    divCadastro.appendChild(pcarga)
+    
+    //Adicionando Vagas
+    pvagas.innerHTML = `Vagas: ${String(vagas.value)}`
+    divCadastro.appendChild(pvagas)
+
+    //Adicionando a divisão
     view.appendChild(hr)
     
 }
