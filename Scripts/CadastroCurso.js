db62717e762d4587387e53a253b22c6fdff7fb52
Rafael leite <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-05-14 13:18:07 -0300

    v 2.9

--- Scripts/CadastroCurso.js
+++ Scripts/CadastroCurso.js
@@ -1,20 +1,15 @@
        
         function confirma(){
-            let nomedocurso = document.getElementById('exampleInputCurso')
-            let view = document.getElementById('aqui')
-            let divCadastro = document.createElement('div')
             let senha = document.getElementById('senha')
-            let pcurso = document.createElement('p')
-            divCadastro.style = "background-color:red;"
-            
-           /*  pcurso.innerHTML = `O curso ${String(nomedocurso.value)} foi adicionado!`
-            view.appendChild(divCadastro)
-            divCadastro.appendChild(pcurso) */
+            let botao = document.getElementById('enviar')
 
             if(String(senha.value) == "Nota100!"){
-                window.alert("Curso cadastrado com Sucesso")
+                botao.onclick = "#"
+                botao.type = "submit"
+                window.alert("Curso Enviado")
             }else{
                 window.alert("Senha Incorreta, tente novamente!")
+                botao.type="#"
             }
           
         }
