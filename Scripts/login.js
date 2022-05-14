7e49f2a5a9a7f38b3bacf066a33f4e187aa08a50
Rafael leite <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-05-14 16:04:04 -0300

    v 4.7

--- Scripts/login.js
+++ Scripts/login.js
@@ -2,12 +2,16 @@ function validar(){
     let email = document.getElementById('email')
     let senha = document.getElementById('senha')
     let link = document.getElementById('link')
+    let termos = document.getElementById('exampleCheck1')
 
-    if(String(email.value) == "secretariaMDES@senai.com" && String(senha.value) == "Nota100!" ){
+    if(String(email.value) == "secretariaMDES@senai.com" && String(senha.value) == "Nota100!"){
      
             link.href = "index.html"
 
-    }else{
+    }else if(Boolean(termos.value) == false){
+        link.href = "#"
+        window.alert("Você precisa concordar com os termos!")
+    } else{
         link.href = "#"
         window.alert("Senha Incorreta!")
     }
