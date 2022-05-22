baa674d66dd4914d88397f30b8559f32e7ea79e5
Rafael leite <69431917+rafaelleitedasilva@users.noreply.github.com>
2022-05-22 13:58:13 -0300

    v 6.0

--- Scripts/ambiente.js
+++ Scripts/ambiente.js
@@ -1,50 +1,14 @@
 var div = document.getElementById('form')
 let sala01 = document.getElementById("sala1")
 
-/* function Sala1(){
-    sala01.className = "selecionado"
-    div.className = "show"
-}
-
-function sala2(){
-    let sala = document.getElementById("sala2")
-    sala.className = "selecionado"
-}
-
-function sala3(){
-    let sala = document.getElementById("sala3")
-    sala.className = "selecionado"
-}
-
-function sala4(){
-    let sala = document.getElementById("sala4")
-    sala.className = "selecionado"
-} */
-
-/* function confirma(){
-    /* let confirma = window.confirm("Você colocou todas as informações?") 
-    if(x === "lab1"){
-        let professor = document.getElementById("professor")
-        let escrita = document.getElementById('lab1')
-
-        escrita.innerText += ` ${String(professor.value)}`
-        sala01.className = "escolhido"
-    } else{
-        window.alert("Não deu certo")
-    }
-} */
-
-let x = 0;
-
 function confirmaSala1(){
-        let professor = document.getElementById("professor")
-        let data = document.getElementById("data")
-        let hora = document.getElementById("hora")
+        let professor = document.getElementById("professor1")
+        let data = document.getElementById("data1")
+        let hora = document.getElementById("hora1")
         let tr = document.createElement('tr')
-        tr.className = x
         let cancela = document.createElement('button')
         cancela.className = "btn-danger"
-        let tbody = document.getElementById("tbody");
+        let tbody = document.getElementById("tbody1");
             
             let tdOne = document.createElement('td')
             tdOne.appendChild(cancela)
@@ -107,11 +71,54 @@ function confirmaSala1(){
             tr.appendChild(tdThree)
             tr.appendChild(tdFour)
             tr.appendChild(tdOne)
-            x++
+}
+
+function confirmaSala2(){
+    let x = 0;
+
+
+    
+
+
+    let professor = document.getElementById("professor2")
+    let data = document.getElementById("data2")
+    let hora = document.getElementById("hora2")
+    let tr = document.createElement('tr')
+    let cancela = document.createElement('button')
+    cancela.className = "btn-danger"
+    let tbody = document.getElementById("tbody2");
         
-        
+        let tdOne = document.createElement('td')
+        tdOne.appendChild(cancela)
+
+        let tdTwo = document.createElement('td')
+        tdTwo.innerHTML = String(professor.value);
+
+        let tdThree = document.createElement('td')
+        tdThree.innerText = String(data.value);
+
+        let tdFour = document.createElement('td')
+        tdFour.innerHTML = String(hora.value);
+
+        cancela.innerText = "X"
+        cancela.onclick = function(){
+            cancela.closest("tr").remove();
         }
-            
+    
+        tbody.appendChild(tr)
+        tr.appendChild(tdTwo)
+        tr.appendChild(tdThree)
+        tr.appendChild(tdFour)
+        tr.appendChild(tdOne)
+        x++
+}
+
+/*     function excluirTudo(){
+        let table = document.getElementById('registros')
+        let linhas = tbody.children
+        tbody.remove(linhas)
+    }
+             */
 
 
 
